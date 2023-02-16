e594e7c44476ba0a3ca8f1010a6dafe58f2c9b03
mrshelile <davertzshelile@gmail.com>
2023-02-16 04:34:01 -0800

    adding routes

--- src/constants/navs.tsx
+++ src/constants/navs.tsx
@@ -1,20 +1,25 @@
 export let navs =[
-    {
+    {   
+        path:"/",
         name:"Home",
     },
-    {
+    {   path:"/about",
         name:"About"
     },
     {
+        path:"/resume",
         name:"Resume",
     },
     {
+        path:"/portfolio",
         name:"Portfolio"
     },
     {
+        path:"/services",
         name:"Services"
     },
     {
+           path:"/contact",
         name:'Contact'
     }
 ]
---
+++ src/pages/Resume/Resume.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Resume() {
+  return (
+    <div>
+      This is Resume Page
+    </div>
+  )
+}
