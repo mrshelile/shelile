0ce91b38d61893b4f568c94b421aa227333cf184
mrshelile <davertzshelile@gmail.com>
2023-02-16 03:58:24 -0800

    home page done

--- src/componets/Footer.tsx
+++ src/componets/Footer.tsx
@@ -1,5 +1,8 @@
 import { Footer } from 'flowbite-react'
-import React from 'react'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 export default function FooterPage() {
   return (
@@ -10,6 +13,20 @@ export default function FooterPage() {
     by="Malefetsane Shelile"
     year={2023}
   />
+  <Footer.LinkGroup>
+    <Footer.Link href="https://github.com/mrshelile">
+      <GitHubIcon></GitHubIcon>
+    </Footer.Link>
+    <Footer.Link href="https://www.linkedin.com/in/malefetsane-shelile-2692531ba">
+      <LinkedInIcon></LinkedInIcon>
+    </Footer.Link>
+    <Footer.Link href="https://www.instagram.com/fatzshelile/">
+      <InstagramIcon></InstagramIcon>
+    </Footer.Link>
+    <Footer.Link href="https://wa.me/+26669356845?text=I'm%20interested%20in%20your%20portfolio">
+      <WhatsAppIcon></WhatsAppIcon>
+    </Footer.Link>
+  </Footer.LinkGroup>
 </Footer>
     </div>
   )
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
-
+import { Button } from 'flowbite-react'
+import dev2 from '../../assets/dev2.svg'
 export default function Home() {
   return (
     <div>
-      <div className="grid grid-cols-2 gap-4">
-        <div>01</div>
-        <div>
-          
+      <div className="grid grid-cols-2 gap-2">
+        <div className='col-span-1'>
+          <div className="grid grid-rows-3 grid-flow-col gap-5 mt-24 ml-20">
+            <div className='text-4xl font-semibold '>
+              <span className='text-cyan-500'>Hi,</span> i'm Malefetsane Shelile
+              but you can call me by my last name.
+            </div>
+            <div className='text-xl font-light'>
+              <span className='text-cyan-500'> I am</span> the most passionate <span className='text-cyan-500'>Computer engineer</span> you will ever work with. If you have a great project
+              that needs  <span className='text-cyan-500'>amazing skills</span>, I'm your guy.
+            </div>
+            <div>
+              <Button className='uppercase' outline={true} gradientDuoTone="cyanToBlue" pill={true}
+              >My Portfolio
+              </Button>
+             </div>
+          </div>
+        </div>
+        <div className='col-span-1'>
+          <img src={dev2} width="90%" />
         </div>
       </div>
     </div>
